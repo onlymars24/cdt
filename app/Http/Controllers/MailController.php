bf3f9736ccf3813d163a2aab569c42d134d4eab5
onlymars24 <marsel.galimov.241@gmail.com>
2024-03-03 15:54:22 +0300

    c6

--- app/Http/Controllers/MailController.php
+++ app/Http/Controllers/MailController.php
@@ -10,6 +10,9 @@ use Illuminate\Support\Facades\Mail;
 class MailController extends Controller
 {
     public function send(Request $request){
-        Mail::to(env('MAIL_ADMIN'))->send(new MailApply($request->name, $request->phone, $request->email));
+        // Mail::to(env('MAIL_ADMIN'))->send(new MailApply($request->name, $request->phone, $request->email));
+        return response([
+            'all' => $request->all()
+        ]);
     }
 }
